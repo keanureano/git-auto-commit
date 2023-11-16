50da0bcef7ccb70adaf79e8204c1c2fd5385c0f5
Keanu Reaño <reanokeanu@gmail.com>
2023-11-16 14:55:53 +0800

    refactor cleanup method

--- preprocess.py
+++ preprocess.py
@@ -7,19 +7,21 @@ def main():
         "Add a feature flag to enable expiration of retry lanes (#27694)",
         "Bump browserify-sign from 4.0.4 to 4.2.2 in /fixtures/expiration (#27600)",
         "Fix: Enable enableUnifiedSyncLane (#27646)",
+        "[Float][Fiber] Fixes incorrect boolean logic around loading states",
     ]
     cleaned_messages = cleanup(messages)
     filtered_messages = filter(cleaned_messages)
-
+    print("Cleaned Messages:", cleaned_messages)
     print("Filtered Messages:", filtered_messages)
 
 
 def cleanup(messages):
     cleaned_messages = []
     for message in messages:
-        message = "".join(char for char in message if char not in "()")
         message = message[message.find(":") + 1 :].strip()
-        message = message[: message.find("#")].strip()
+        message = message[message.find("]") + 1 :].strip()
+        message = message[message.find("]") + 1 :].strip()
+        message = message[: message.find("(")].strip()
         message = message.lower()
         cleaned_messages.append(message)
     return cleaned_messages
