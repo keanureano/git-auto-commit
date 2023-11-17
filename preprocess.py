85506cfbd6d6ceb690b6905f179aa96230ab6b3a
Keanu Reaño <reanokeanu@gmail.com>
2023-11-17 08:28:26 +0800

    refactor filtering method and put bot filter on top

--- preprocess.py
+++ preprocess.py
@@ -26,7 +26,7 @@ def cleanup(dataset):
     cleaned_dataset = []
     for data in dataset:
         cleaned_data = {"message": "", "diff": ""}
-        message = data.get("message", "")
+        message = data.get("message")
 
         # Keep only the first line of commit message
         message = (
@@ -46,11 +46,11 @@ def cleanup(dataset):
         # Remove parentheses and their contents
         message = re.sub(r"\(.*?\)", "", message)
 
-        # Strip and lowercaste message
+        # Strip and lowercase message
         message = message.strip().lower()
 
         # Truncate the diff, will filter this later so it doesn't matter if it's truncated
-        diff = data.get("diff", "")
+        diff = data.get("diff")
         diff = diff[:10000].strip()
 
         print("Cleaned", message)
@@ -79,6 +79,11 @@ def filter(dataset):
         message = data.get("message")
         diff = data.get("diff")
 
+        # Check for bot commits
+        if message.split()[0] in ["merge", "bump"]:
+            filtered_dataset["bot_commits"].append(message)
+            continue
+
         # Check for short messages
         if len(message) < 50:
             filtered_dataset["short_messages"].append(message)
@@ -89,26 +94,20 @@ def filter(dataset):
             filtered_dataset["large_diffs"].append(message)
             continue
 
-        # Check for bot commits
-        if message.split()[0].lower() in ["merge", "bump"]:
-            filtered_dataset["bot_commits"].append(message)
-            continue
-
         spacy_doc = spacy_nlp(message)
         stanza_doc = stanza_nlp(message)
         spacy_first = spacy_doc[0]
         stanza_first = stanza_doc.sentences[0].words[0]
 
         # Check for verb
-        if not (spacy_first.pos_ == "VERB" and stanza_first.upos == "VERB"):
+        if not (spacy_first.pos_ == "VERB" or stanza_first.upos == "VERB"):
             filtered_dataset["non_verbs"].append(message)
             continue
 
         # Check for imperative
         if not (
             "Inf" in spacy_first.morph.get("VerbForm")
-            and stanza_first.deprel == "root"
-            and "Mood=Imp" in stanza_first.feats
+            or any(feat in stanza_first.feats for feat in ["Mood=Imp", "VerbForm=Inf"])
         ):
             filtered_dataset["non_imperatives"].append(message)
             continue
