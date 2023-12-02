4c390a2dd37443141da4fe3d91530b14f6cd6f2a
Keanu Reaño <reanokeanu@gmail.com>
2023-12-02 17:16:50 +0800

    analysis using meteor metric

--- analyse_accuracy.py
+++ analyse_accuracy.py
@@ -1,11 +1,65 @@
-from datasets import load_metric
+import json
+import matplotlib.pyplot as plt
+import numpy as np
+from nltk.translate import meteor
+from nltk.tokenize import word_tokenize
+import nltk
 
-bleu = load_metric("bleu")
-txt = "Fixed multiple small memory leaks. Click events are handled by React DOM more reliably in mobile browsers, particularly in Mobile Safari. Input events are handled more reliably in IE 10 and IE 11; spurious events no longer fire when using a placeholder. React DOM now supports the `cite` and `profile` HTML attributes. React DOM now supports the `onAnimationStart`, `onAnimationEnd`, `onAnimationIteration`, `onTransitionEnd`, and `onInvalid` events. Support for `onLoad` has been added to `object` elements.", "input": [{"role": "system", "content": "Summarize diff logs with a commit message."}, {"role": "user", "content": "diff --git a/docs/_posts/2016-03-07-react-v15-rc1.md b/docs/_posts/2016-03-07-react-v15-rc1.md\nindex 3eb8fb296fcf..932ae0988700 100644\n--- a/docs/_posts/2016-03-07-react-v15-rc1.md\n+++ b/docs/_posts/2016-03-07-react-v15-rc1.md\n@@ -87,7 +87,6 @@ Each of these changes will continue to work as before with a new warning until t\n ### Notable bug fixes\n \n - Fixed multiple small memory leaks\n-- Click events are handled by React DOM more reliably in mobile browsers, particularly in Mobile Safari.\n - Input events are handled more reliably in IE 10 and IE 11; spurious events no longer fire when using a placeholder.\n - React DOM now supports the `cite` and `profile` HTML attributes.\n - React DOM now supports the `onAnimationStart`, `onAnimationEnd`, `onAnimationIteration`, `onTransitionEnd`, and `onInvalid` events. Support for `onLoad` has been added to `object` elements."
-x = txt.split()
-txt2 = "remove 15 rc1 changelog entry that was fixed in 0.14"
-y = txt.split()
-predictions = [[x]]
-references = [[[y]]]
+def main():
+    nltk.download("punkt")
+    nltk.download("wordnet")
+    base_dataset = load_dataset("base_model_results")
+    finetuned_dataset = load_dataset("finetuned_model_results")
+    prompt_engineered_dataset = load_dataset("prompt_engineered_model_results")
 
-bleu.compute(predictions=predictions, references=references)
+    base_scores = evaluate_accuracy(base_dataset, "Base")
+    finetuned_scores = evaluate_accuracy(finetuned_dataset, "Finetuned")
+    prompt_engineered_scores = evaluate_accuracy(prompt_engineered_dataset, "Prompt Engineered")
+
+    smoothness_factor = 50  # You can adjust this parameter for more or less smoothing
+
+    plot_scores(
+        ["Base", "Finetuned", "Prompt Engineered"],
+        [base_scores, finetuned_scores, prompt_engineered_scores],
+        smoothness_factor
+    )
+
+def load_dataset(data_path):
+    with open(f"test_results/{data_path}.jsonl", "r", encoding="utf-8") as f:
+        dataset = [json.loads(line) for line in f]
+    return dataset
+
+def evaluate_accuracy(dataset, label):
+    meteor_scores = []
+
+    for data in dataset:
+        reference = word_tokenize(data["reference"])
+        candidate = word_tokenize(data["candidate"])
+
+        # Calculate METEOR score
+        meteor_score = meteor([reference], candidate)
+        meteor_scores.append(meteor_score)
+
+    # Calculate and return the average METEOR score for the dataset
+    average_meteor_score = sum(meteor_scores) / len(meteor_scores)
+    print(f"{label} Average METEOR Score: {average_meteor_score:.4f}")
+    return meteor_scores
+
+def moving_average(data, window_size):
+    return np.convolve(data, np.ones(window_size)/window_size, mode='valid')
+
+def plot_scores(labels, scores, smoothness_factor):
+    plt.figure(figsize=(10, 6))
+    
+    for i in range(len(labels)):
+        smoothed_scores = moving_average(scores[i], smoothness_factor)
+        plt.plot(smoothed_scores, label=labels[i])
+
+    plt.title("Smoothed METEOR Scores for Different Datasets")
+    plt.xlabel("Sample")
+    plt.ylabel("METEOR Score")
+    plt.legend()
+    plt.show()
+
+if __name__ == "__main__":
+    main()
