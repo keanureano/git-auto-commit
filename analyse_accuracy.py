2219824716c9839000e375e61fc0f2cf5675e709
Keanu Reaño <reanokeanu@gmail.com>
2023-12-02 17:37:51 +0800

    rogue score

--- analyse_accuracy.py
+++ analyse_accuracy.py
@@ -1,8 +1,7 @@
 import json
 import matplotlib.pyplot as plt
 import numpy as np
-from nltk.translate.bleu_score import sentence_bleu
-
+from rouge import Rouge
 
 def main():
     # You can adjust this parameter for more or less smoothing
@@ -13,9 +12,7 @@ def main():
 
     base_scores = evaluate_accuracy(base_dataset, "Base")
     finetuned_scores = evaluate_accuracy(finetuned_dataset, "Finetuned")
-    prompt_engineered_scores = evaluate_accuracy(
-        prompt_engineered_dataset, "Prompt Engineered"
-    )
+    prompt_engineered_scores = evaluate_accuracy(prompt_engineered_dataset, "Prompt Engineered")
 
     plot_scores(
         ["Base", "Finetuned", "Prompt Engineered"],
@@ -23,54 +20,50 @@ def main():
         smoothness_factor,
     )
 
-
 def load_dataset(data_path):
     with open(f"test_results/{data_path}.jsonl", "r", encoding="utf-8") as f:
         dataset = [json.loads(line) for line in f]
     return dataset
 
-
 def evaluate_accuracy(dataset, label):
-    bleu_scores = []
+    rouge_scores = []
 
     for data in dataset:
-        reference = data["reference"].split()
-        candidate = data["candidate"].split()
+        reference = data["reference"]
+        candidate = data["candidate"]
 
-        # Calculate BLEU score using sentence_bleu
-        bleu_score = calculate_bleu(reference, candidate)
-        bleu_scores.append(bleu_score)
+        # Calculate ROUGE-1 score
+        rouge_score = calculate_rouge(reference, candidate)
+        rouge_scores.append(rouge_score)
 
-    # Calculate and print the average BLEU score for the dataset
-    average_bleu_score = sum(bleu_scores) / len(bleu_scores)
-    print(f"{label} Average BLEU Score: {average_bleu_score:.4f}")
-    return bleu_scores, average_bleu_score
+    # Calculate and print the average ROUGE score for the dataset
+    average_rouge_score = sum(rouge_scores) / len(rouge_scores)
+    print(f"{label} Average ROUGE Score: {average_rouge_score:.4f}")
+    return rouge_scores, average_rouge_score
 
-
-def calculate_bleu(reference, candidate):
-    # Calculate BLEU score using sentence_bleu
-    bleu_score = sentence_bleu([reference], candidate, weights=(1, 0, 0, 0))
-    return bleu_score
-
+def calculate_rouge(reference, candidate):
+    # Calculate ROUGE-1 score using the rouge library
+    rouge = Rouge()
+    scores = rouge.get_scores(candidate, reference)
+    rouge_score = scores[0]["rouge-1"]["f"]
+    return rouge_score
 
 def moving_average(data, window_size):
     return np.convolve(data, np.ones(window_size) / window_size, mode="valid")
-
 
 def plot_scores(labels, scores, smoothness_factor):
     plt.figure(figsize=(10, 6))
 
     for i in range(len(labels)):
-        bleu_scores, average_bleu_score = scores[i]
-        smoothed_scores = moving_average(bleu_scores, smoothness_factor)
-        plt.plot(smoothed_scores, label=f"{labels[i]} (Avg: {average_bleu_score:.4f})")
+        rouge_scores, average_rouge_score = scores[i]
+        smoothed_scores = moving_average(rouge_scores, smoothness_factor)
+        plt.plot(smoothed_scores, label=f"{labels[i]} (Avg: {average_rouge_score:.4f})")
 
-    plt.title("Smoothed BLEU Scores for Different Models")
+    plt.title("Smoothed ROUGE-1 Scores for Different Models")
     plt.xlabel("Sample")
-    plt.ylabel("BLEU Score")
+    plt.ylabel("ROUGE-1 Score")
     plt.legend()
     plt.show()
-
 
 if __name__ == "__main__":
     main()
